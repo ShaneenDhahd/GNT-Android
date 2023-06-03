61fe8933504070b298c9e336f26baf8be44ca2d3
Shaneen Dhahd <shaneendhahd@gmail.com>
2023-06-03 19:03:59 +0300

    fixes.1

--- app/src/main/java/shaneen/dhahd/gnt_test/ui/LoginFragment.kt
+++ app/src/main/java/shaneen/dhahd/gnt_test/ui/LoginFragment.kt
@@ -10,15 +10,20 @@ import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import shaneen.dhahd.gnt_test.R
 import shaneen.dhahd.gnt_test.databinding.FragmentLoginBinding
+import shaneen.dhahd.gnt_test.ext.isEmpty
 import shaneen.dhahd.gnt_test.ext.navigateTo
 import shaneen.dhahd.gnt_test.network.wrappers.ResponseWrapper
 import shaneen.dhahd.gnt_test.viewModel.LoginViewModel
+import kotlin.math.log
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
     private val loginVM: LoginViewModel by viewModels()
+
+    var email = ""
+    var password = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,13 +39,32 @@ class LoginFragment : Fragment() {
         binding.apply {
             loginBtn.setOnClickListener {
                 loginVM.apply {
-                    login(emailField.text.toString(), passwordField.text.toString())
+                    validate {
+                        login(email, password)
+                    }
                     loginObservable.observe(viewLifecycleOwner) {
                         when(it){
                             is ResponseWrapper.Success -> findNavController().navigateTo(R.id.navigation_forms, true)
                             else -> {}
                         }
                     }
+                }
+            }
+        }
+    }
+
+    private fun validate(login: () -> Unit) {
+        binding.apply {
+            if (!emailField.text.isNullOrBlank() && !passwordField.text.isNullOrBlank()) {
+                email = emailField.text.toString()
+                password = passwordField.text.toString()
+                login()
+            } else {
+                if (!emailField.isEmpty()) {
+                    emailField.error = "Email can not be empty"
+                }
+                if (!passwordField.isEmpty()) {
+                    passwordField.error = "Password can not be empty"
                 }
             }
         }
--- app/src/main/java/shaneen/dhahd/gnt_test/ui/MainActivity.kt
+++ app/src/main/java/shaneen/dhahd/gnt_test/ui/MainActivity.kt
@@ -23,46 +23,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_graph) as? NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_graph) as? NavHostFragment
         navHostFragment?.also {
             val navController = navHostFragment.navController
-
-            // Set up ActionBar with Navigation
             setupActionBarWithNavController(navController)
         }
-
-            // Handle the case when the retrieved fragment is null or not a NavHostFragment
-//        binding.fragmentsContainer
-//
-//        navigateToLogin()
-//        loginVM.apply {
-//            refreshToken()
-//            loginObservable.observe(this@MainActivity) {
-//                binding.apply {
-//                    welcomeMsg.visibility = View.GONE
-//                    progressBar.visibility = View.GONE
-//                }
-//                when(it){
-//                    is ResponseWrapper.Failure -> navigateToLogin()
-//                    is ResponseWrapper.LocalFailure -> this@MainActivity.toast("No Connection")
-//                    is ResponseWrapper.Success -> navigateToForm()
-//                    else -> {}
-//                }
-//            }
-//        }
     }
-
-//    private fun navigateToLogin(){
-//        val loginFragment = LoginFragment()
-//        supportFragmentManager.beginTransaction()
-//            .replace(binding.fragmentsContainer.id, loginFragment)
-//            .commit()
-//    }
-//     private fun navigateToForm(){
-//            val formFragment = FormFragment()
-//            supportFragmentManager.beginTransaction()
-//                .replace(binding.fragmentsContainer.id, formFragment)
-//                .commit()
-//        }
 
 }
